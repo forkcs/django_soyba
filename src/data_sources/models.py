7b9a3d4899cb13be39dac8a0e1b12e3a9fb6f600
Fedor Soldatkin <forkcs@yandex.ru>
2023-04-09 00:55:44 +0300

    Remove MarketDataSource.description

--- src/data_sources/models.py
+++ src/data_sources/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class MarketDataSource(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
 
     class Meta:
         constraints = [
