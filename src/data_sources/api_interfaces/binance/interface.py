08f30c5498b9f1502644341e289879a109853256
woowgers <sashashibaev@gmail.com>
2023-04-09 02:03:40 +0300

    add Timeframe.construct factory

--- src/data_sources/api_interfaces/binance/interface.py
+++ src/data_sources/api_interfaces/binance/interface.py
@@ -42,4 +42,5 @@ class BinanceInterface(DataSourceInterface):
         return instruments
 
     def get_available_timeframes(self) -> Iterable[Timeframe]:
-        return ("1s", "1m", "3m", "5m", "15m", "30m", "1h", "2h", "4h", "6h", "8h", "12h", "1d", "3d", "1w", "1M")
+        TIMEFRAMES = ("1s", "1m", "3m", "5m", "15m", "30m", "1h", "2h", "4h", "6h", "8h", "12h", "1d", "3d", "1w", "1M")
+        return map(Timeframe.construct, TIMEFRAMES)
