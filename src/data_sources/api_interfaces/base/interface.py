42b35ac04c6908edd8747f92f2da38c02d6505eb
Fedor Soldatkin <forkcs@yandex.ru>
2023-04-09 00:55:24 +0300

    Extend base api interface

--- src/data_sources/api_interfaces/base/interface.py
+++ src/data_sources/api_interfaces/base/interface.py
@@ -7,3 +7,9 @@ from data_sources.api_interfaces.schema import OHLC, Timeframe
 class DataSourceInterface:
     def get_ohlc(self, *, symbol: str, timeframe: Timeframe, count: int, start_datetime: datetime) -> Iterable[OHLC]:
         raise NotImplementedError
+
+    def get_available_instruments(self) -> Iterable[str]:
+        raise NotImplementedError
+
+    def get_available_timeframes(self) -> Iterable[Timeframe]:
+        raise NotImplementedError
