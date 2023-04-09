210706454a54dd0601c159d3e8623e237e7ebb20
woowgers <sashashibaev@gmail.com>
2023-04-09 19:30:56 +0300

    add `float` to possible field type of `KrakenInstrument`

--- src/data_sources/api_interfaces/kraken/types.py
+++ src/data_sources/api_interfaces/kraken/types.py
@@ -1,6 +1,5 @@
-from dataclasses import dataclass
 from typing import TypeAlias
 
 KrakenOHLC: TypeAlias = tuple[int, str, str, str, str, str, str, int]
 
-KrakenInstrument: TypeAlias = dict[str, str | int]
+KrakenInstrument: TypeAlias = dict[str, str | int | float]
