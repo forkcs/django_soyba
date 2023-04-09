fd1478d47e91a55664dfb73a1b41845c433be915
woowgers <sashashibaev@gmail.com>
2023-04-09 19:26:19 +0300

    add basic `KrakenInstrument` dataclass

--- src/data_sources/api_interfaces/kraken/types.py
+++ src/data_sources/api_interfaces/kraken/types.py
@@ -1,4 +1,6 @@
+from dataclasses import dataclass
 from typing import TypeAlias
 
-
 KrakenOHLC: TypeAlias = tuple[int, str, str, str, str, str, str, int]
+
+KrakenInstrument: TypeAlias = dict[str, str | int]
