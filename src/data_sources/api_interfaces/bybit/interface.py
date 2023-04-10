68dcc7e60bbb96090d825de49e8ee260e8ff4f59
woowgers <sashashibaev@gmail.com>
2023-04-10 21:10:03 +0300

    bybit WIP

---
+++ src/data_sources/api_interfaces/bybit/interface.py
@@ -0,0 +1,62 @@
+import os
+from datetime import datetime
+from decimal import Decimal
+
+from pybit.unified_trading import HTTP
+
+from data_sources.api_interfaces.bybit.types import BybitOHLC
+
+from ..base.interface import DataSourceInterface
+from ..schema import OHLC, Timeframe, TimeframeUnit
+
+
+class BybitInterface(DataSourceInterface):
+    def __init__(self):
+        self.session = HTTP(api_key=os.getenv("BYBIT_API_KEY"), api_secret=os.getenv("BYBIT_API_SECRET"))
+        ...
+
+    @staticmethod
+    def _construct_ohlc(raw_ohlc: BybitOHLC) -> OHLC:
+        return OHLC(
+            open=Decimal(raw_ohlc[1]),
+            high=Decimal(raw_ohlc[2]),
+            low=Decimal(raw_ohlc[3]),
+            close=Decimal(raw_ohlc[4]),
+            start_time=datetime.fromtimestamp(float(raw_ohlc[0])),
+        )
+
+    @staticmethod
+    def _minutes_from_timeframe(timeframe: Timeframe) -> float:
+        MINUTE_BY_UNIT: dict[TimeframeUnit, float] = {
+            TimeframeUnit.SECOND: 1 / 60.0,
+            TimeframeUnit.MINUTE: 1.0,
+            TimeframeUnit.HOUR: 60.0,
+            TimeframeUnit.DAY: 24 * 60.0,
+            TimeframeUnit.WEEK: 7 * 24 * 60.0,
+            TimeframeUnit.MONTH: 30 * 24 * 60.0,
+            TimeframeUnit.YEAR: 356 * 24 * 60.0,
+        }
+        return float(timeframe.count * MINUTE_BY_UNIT[timeframe.unit])
+
+    def get_ohlc(self, *, symbol: str, timeframe: Timeframe, count: int, start_datetime: datetime) -> tuple[OHLC, ...]:
+        minutes: float = self._minutes_from_timeframe(timeframe)
+        raw_ohlc_list = self.session.get_kline(category='spot', symbol=symbol, interval=f'{minutes}')
+        return tuple(map(self._construct_ohlc, raw_ohlc_list))
+
+    def get_available_instruments(self) -> tuple[str, ...]:
+        ...
+
+    def get_available_timeframes(self) -> tuple[Timeframe, ...]:
+        """There are also days (D), weeks (W) and months (M), not sure how to return them."""
+        return (
+            Timeframe(count=1, unit=TimeframeUnit.MINUTE),
+            Timeframe(count=3, unit=TimeframeUnit.MINUTE),
+            Timeframe(count=5, unit=TimeframeUnit.MINUTE),
+            Timeframe(count=15, unit=TimeframeUnit.MINUTE),
+            Timeframe(count=30, unit=TimeframeUnit.MINUTE),
+            Timeframe(count=60, unit=TimeframeUnit.MINUTE),
+            Timeframe(count=120, unit=TimeframeUnit.MINUTE),
+            Timeframe(count=240, unit=TimeframeUnit.MINUTE),
+            Timeframe(count=360, unit=TimeframeUnit.MINUTE),
+            Timeframe(count=720, unit=TimeframeUnit.MINUTE),
+        )
---
+++ src/data_sources/api_interfaces/bybit/types.py
@@ -0,0 +1,3 @@
+from typing import TypeAlias
+
+BybitOHLC: TypeAlias = tuple[str, str, str, str, str, str, str]
