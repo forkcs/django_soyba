3cf1d1facac349dd221ec7f9525edd1a6bcc3b9e
woowgers <sashashibaev@gmail.com>
2023-04-14 03:12:01 +0300

    add necessary `Meta.fields` to serializers

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -7,8 +7,10 @@ from market_data.models.ohlc import OHLC
 class InstrumentSerializer(serializers.Serializer):
     class Meta:
         model = Instrument
+        fields = '__all__'
 
 
 class OHLCSerializer(serializers.Serializer):
     class Meta:
         model = OHLC
+        fields = '__all__'
