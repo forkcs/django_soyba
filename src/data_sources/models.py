530a59d647c3e508f7cd92a6aa55bd92c8d645be
Fedor Soldatkin <forkcs@yandex.ru>
2023-04-22 15:20:42 +0300

    Remove redundant method

--- src/data_sources/models.py
+++ src/data_sources/models.py
@@ -1,5 +1,3 @@
-from functools import reduce
-
 from django.db import models
 
 
@@ -7,8 +5,3 @@ class MarketDataSourceChoices(models.TextChoices):
     BINANCE = 'binance', 'Binance'
     KRAKEN = 'kraken', 'Kraken'
     BYBIT = 'bybit', 'Bybit'
-
-    @property
-    @classmethod
-    def options(cls) -> tuple[str, ...]:
-        return reduce(lambda tup, pair: tup + pair, MarketDataSourceChoices.choices)
